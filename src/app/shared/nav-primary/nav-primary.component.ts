5862f32d5096ee23a172490893dcad22870a5e80
Kasey Labelle <labellk@Kaseys-MacBook-Pro.local>
2021-06-18 13:52:06 -0400

    code clean up

--- src/app/shared/nav-primary/nav-primary.component.ts
+++ src/app/shared/nav-primary/nav-primary.component.ts
@@ -1,26 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.nav-primary',
+  selector: '.navbar-nav-primary',
   templateUrl: './nav-primary.component.html'
 })
 export class NavPrimaryComponent implements OnInit {
 
   navPrimaryItems = [
-    { title: 'Overview' },
-    {
-      title: 'Components',
-      children: [
-        {
-          linkText: 'Breadcrumbs',
-          url: './components/breadcrumbs'
-        },
-        {
-          linkText: 'Call to Action',
-          url: './components/call-to-action'
-        }
-      ]
-    }
+    { linkText: 'Breadcrumbs', url: './components/breadcrumbs' },
+    { linkText: 'Call to Action', url: './components/call-to-action' }
   ];
 
   constructor() { }
