0abdecc0840170da6872d0aff29ac9d018865111
Kasey Labelle <labellk@Kaseys-MacBook-Pro.local>
2021-11-15 18:58:31 -0500

    add link component

---
+++ src/docs/components/variations/links/Ghost.tsx
@@ -0,0 +1,9 @@
+import Link from '../../../../library/components/Link';
+
+const Ghost = () => {
+  return <>
+    <Link href="#" variant="ghost">Ghost Link</Link>
+  </>;
+}
+
+export default Ghost;
---
+++ src/library/components/Link.tsx
@@ -0,0 +1,33 @@
+// TODO: do we want to use React's <Link> here? Will React's link mess with our link?
+
+import { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  href: string;
+  variant?: string;
+  [key: string]: any;
+}
+
+const Link = (props: Props) => {
+  const { variant, href, ...linkAttrs } = props;
+
+  let classes = '';
+  switch (variant) {
+    case 'subtle':
+      classes = 'ads-link-subtle'
+      break;
+    case 'ghost':
+      classes = 'ads-link-ghost'
+      break;
+    default:
+      classes = 'ads-link';
+      break;
+  }
+
+  return (
+    <a href={props.href} className={classes} {...linkAttrs}>{props.children}</a>
+  );
+}
+
+export default Link;
