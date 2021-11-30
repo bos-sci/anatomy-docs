2bbb2dd9e08b96f7388451ed42ef1d1327b84d55
ajohns <ashleypjohns@gmail.com>
2021-11-30 12:00:51 -0500

    address PR review

--- src/docs/components/variations/inputCheckboxes/CheckboxListError.tsx
+++ src/docs/components/variations/inputCheckboxes/CheckboxListError.tsx
@@ -4,7 +4,7 @@ const CheckboxListError = () => {
   return (
     <fieldset className="ads-fieldset" aria-describedby="listErrorMessage" aria-invalid="true">
       <legend className="ads-legend">Legend</legend>
-      <p id="listErrorMessage" className="ads-input-error">Error Message.</p>
+      <p id="listErrorMessage" className="ads-input-error">Error Message</p>
       <InputCheckbox label="Checkbox 1" checked/>
       <InputCheckbox label="Checkbox 2" />
       <InputCheckbox label="Checkbox 3" />
--- src/docs/components/variations/inputCheckboxes/WithError.tsx
+++ src/docs/components/variations/inputCheckboxes/WithError.tsx
@@ -1,7 +1,7 @@
 import InputCheckbox from '../../../../library/components/InputCheckbox';
 
 const WithError = () => {
-  return <InputCheckbox label="Error checkbox" errorText="Error message." />;
+  return <InputCheckbox label="Error checkbox" errorText="Error message" />;
 }
 
 export default WithError;
