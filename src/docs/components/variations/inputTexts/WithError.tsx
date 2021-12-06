d7fd8d537a22376121f46f1d7873a03a02e0e5d0
ajohns <ashleypjohns@gmail.com>
2021-12-06 16:09:25 -0500

    Add validation to text inputs

--- src/docs/components/variations/inputTexts/WithError.tsx
+++ src/docs/components/variations/inputTexts/WithError.tsx
@@ -1,7 +1,19 @@
+import { ChangeEvent, useState } from 'react';
 import InputText from '../../../../library/components/InputText';
 
 const WithError = () => {
-  return <InputText label="Input with help text" placeholder="Placeholder text" errorText="Error message" />;
+
+  const [errorText, setErrorText] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.includes(' ')) {
+      setErrorText('This field can\'t contain spaces.');
+    } else {
+      setErrorText('');
+    }
+  }
+
+  return <InputText label="Input with error text" placeholder="Placeholder text" errorText={errorText} onChange={handleChange} />;
 }
 
 export default WithError;
