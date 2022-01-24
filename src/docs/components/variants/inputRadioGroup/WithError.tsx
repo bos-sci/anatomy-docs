92cae024fcdf9a8d2c41498c40a8e6be04bbe47a
ajohns <ashleypjohns@gmail.com>
2022-01-24 12:48:31 -0500

    Add missing space

--- src/docs/components/variants/inputRadioGroup/WithError.tsx
+++ src/docs/components/variants/inputRadioGroup/WithError.tsx
@@ -10,7 +10,7 @@ const WithError = (): JSX.Element => {
     <Example>
       <form onSubmit={handleSubmit}>
         <RadioGroup legend="Legend">
-          <InputRadio label="Radio 1" name="groupRequired" value="errorRequired1" required forceValidation={true}/>
+          <InputRadio label="Radio 1" name="groupRequired" value="errorRequired1" required forceValidation={true} />
           <InputRadio label="Radio 2" name="groupRequired" value="errorRequired2" required />
         </RadioGroup>
       </form>
