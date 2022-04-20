2bd6dd3b6a0da424a4f5e76ca90d10c4362423f3
Kasey Labelle <kasey.l.labelle@gmail.com>
2022-04-20 13:32:30 -0400

    change example href attr value to fix build

--- src/docs/components/variants/skipLink/DefaultSkipLink.tsx
+++ src/docs/components/variants/skipLink/DefaultSkipLink.tsx
@@ -8,11 +8,11 @@ const DefaultSkipLink = (): JSX.Element => {
       <SkipLink destinationId="mainContent" destination='main content'/>
       <header className="docs-wire-header">
         <p>Header area</p>
-        <a href="#">Example header link</a>
+        <a href="/">Example header link</a>
       </header>
       <main id="mainContent" className="docs-wire-main">
         <p>Main content area</p>
-        <a href="#">Example link</a>
+        <a href="/">Example link</a>
       </main>
     </>
   );
