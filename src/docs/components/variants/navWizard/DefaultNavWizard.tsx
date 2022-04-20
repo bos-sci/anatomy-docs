a69d1f7f4ba21ac468d2b29c47236c93a6529466
APJohns <mail@ashpjohns.com>
2022-04-20 14:09:05 -0400

    add margin to demo link

--- src/docs/components/variants/navWizard/DefaultNavWizard.tsx
+++ src/docs/components/variants/navWizard/DefaultNavWizard.tsx
@@ -2,9 +2,11 @@ import NavWizard from '../../../../library/components/navigation/navWizard/NavWi
 import structure from './structure';
 
 const DefaultNavWizard = (): JSX.Element => {
-  return <div>
-    <NavWizard firstTitle="First step title" navItems={structure} />
-  </div>;
+  return (
+    <div className="demo-standalone">
+      <NavWizard firstTitle="First step title" navItems={structure} />
+    </div>
+  );
 }
 
 export default DefaultNavWizard;
