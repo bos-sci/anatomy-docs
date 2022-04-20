a3b143b63bd9ed626120b0177942f8999a528fd4
APJohns <mail@ashpjohns.com>
2022-04-20 10:17:45 -0400

    remove contextual example

--- src/docs/components/variants/navWizard/DefaultNavWizard.tsx
+++ src/docs/components/variants/navWizard/DefaultNavWizard.tsx
@@ -1,11 +1,9 @@
 import NavWizard from '../../../../library/components/navigation/navWizard/NavWizard';
 import structure from './structure';
-import structureContextual from './structureContextual';
 
 const DefaultNavWizard = (): JSX.Element => {
   return <div>
     <NavWizard firstTitle="First step title" navItems={structure} />
-    <NavWizard firstTitle="How can we help you?" navItems={structureContextual} />
   </div>;
 }
 
