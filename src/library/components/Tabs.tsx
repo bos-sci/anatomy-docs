a5e90a19a17c75c89d085953668455fb56894ae4
APJohns <mail@ashpjohns.com>
2022-08-17 13:59:38 -0400

    Add overflow to tabs

--- src/library/components/Tabs.tsx
+++ src/library/components/Tabs.tsx
@@ -5,9 +5,13 @@ import {
   KeyboardEvent,
   ReactElement,
   RefObject,
+  useCallback,
   useEffect,
+  useRef,
   useState,
 } from "react";
+import IconChevronLeft from './icon/icons/IconChevronLeft';
+import IconChevronRight from './icon/icons/IconChevronRight';
 import Tab from "./Tab";
 
 type Props = {
@@ -21,6 +25,65 @@ const Tabs = ({ children }: Props): JSX.Element => {
   const [tabPanelId, setTabPanelId] = useState("");
   const [tabPanels, setTabPanels] = useState<ReactElement[]>([]);
   const [tabRefs, setTabRefs] = useState<RefObject<HTMLButtonElement>[]>([]);
+  const [hasOverflow, setHasOverflow] = useState(false);
+  const [visibleRange, setVisibleRange] = useState([0, 0]);
+  const [scrollBtnStates, setScrollBtnStates] = useState([false, false]); // [leftScrollBtn, rightScrollBtn] disabled when true
+
+  const tabListRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (tabListRef.current) {
+      setHasOverflow(tabListRef.current.scrollWidth > tabListRef.current.clientWidth);
+    }
+  }, [tabListRef.current?.scrollWidth]);
+
+  const handleScroll = () => {
+    if (tabListRef.current) {
+      updateRange();
+      // Disable controls when scrolled all the way left or right
+      if (tabListRef.current?.scrollLeft === 0) {
+        setScrollBtnStates([true, false]);
+      } else if (tabListRef.current?.scrollLeft === tabListRef.current?.scrollWidth - tabListRef.current?.clientWidth) {
+        setScrollBtnStates([false, true]);
+      } else {
+        setScrollBtnStates([false, false]);
+      }
+    }
+  }
+
+  // Determines the index of the first and last visible tab
+  // A tab is deemed visible if 2/3 of it are in the scroll area
+  const updateRange = useCallback(() => {
+    const tabsVisibility = tabRefs.map(ref => {
+      const rightEdge = ref.current?.offsetLeft! + (ref.current?.clientWidth! * 0.66);
+      const leftEdge = ref.current?.offsetLeft! + (ref.current?.clientWidth! * 0.33);
+      return rightEdge < tabListRef.current?.scrollLeft! + tabListRef.current?.clientWidth! && leftEdge > tabListRef.current?.scrollLeft!;
+    });
+    setVisibleRange([tabsVisibility.indexOf(true), tabsVisibility.lastIndexOf(true)]);
+  }, [tabRefs])
+
+  useEffect(() => {
+    updateRange();
+  }, [updateRange]);
+
+  // Scrolls to the tab "distance" tabs away from the left or right most visible tab
+  const scrollTabs = (distance: number) => {
+    let currentTarget = distance < 0 ? visibleRange[0] : visibleRange[1];
+    let newTarget = 0;
+    if (currentTarget + distance > tabRefs.length - 1) {
+      newTarget = 0;
+    } else if (currentTarget + distance < 0) {
+      newTarget = tabRefs.length - 1;
+    } else {
+      newTarget = currentTarget + distance;
+    }
+
+    tabRefs[newTarget].current?.scrollIntoView({
+      behavior: window.matchMedia("(prefers-reduced-motion: no-preference)").matches ? 'smooth' : 'auto',
+      block: 'nearest',
+      inline: distance < 0 ? 'start' : 'end'
+    });
+  }
 
   const selectTab = (index: number) => {
     const tabRef = tabRefs[index];
@@ -29,10 +92,10 @@ const Tabs = ({ children }: Props): JSX.Element => {
   };
 
   const keyManager = (e: KeyboardEvent<HTMLDivElement>) => {
-    e.preventDefault();
     let nextTab = 0;
     switch (e.key) {
       case "ArrowLeft":
+        e.preventDefault();
         if (selectedTab === 0) {
           nextTab = tabPanels.length - 1;
         } else {
@@ -42,6 +105,7 @@ const Tabs = ({ children }: Props): JSX.Element => {
         break;
 
       case "ArrowRight":
+        e.preventDefault();
         if (selectedTab === tabPanels.length - 1) {
           nextTab = 0;
         } else {
@@ -51,10 +115,12 @@ const Tabs = ({ children }: Props): JSX.Element => {
         break;
 
       case "Home":
+        e.preventDefault();
         selectTab(0);
         break;
 
       case "End":
+        e.preventDefault();
         selectTab(tabPanels.length - 1);
         break;
 
@@ -83,18 +149,40 @@ const Tabs = ({ children }: Props): JSX.Element => {
 
   return (
     <div className="bsds-tabs">
-      <div className="bsds-tab-list" role="tablist" onKeyDown={keyManager}>
-        {tabPanels.map((tabPanel, index) => (
-          <Tab
-            key={`${tabPanelId + index}Tab`}
-            tabName={tabPanel.props.tabName}
-            index={index}
-            setSelectedTab={setSelectedTab}
-            isActive={index === selectedTab}
-            tabPanelId={tabPanelId + index}
-            tabRef={tabRefs[index]}
-          />
-        ))}
+      <div className="bsds-tab-controls">
+        <div ref={tabListRef} className="bsds-tab-list" role="tablist" onKeyDown={keyManager} onScroll={handleScroll}>
+          {tabPanels.map((tabPanel, index) => (
+            <Tab
+              key={`${tabPanelId + index}Tab`}
+              tabName={tabPanel.props.tabName}
+              index={index}
+              setSelectedTab={setSelectedTab}
+              isActive={index === selectedTab}
+              tabPanelId={tabPanelId + index}
+              tabRef={tabRefs[index]}
+            />
+          ))}
+        </div>
+        {hasOverflow &&
+          <div className="bsds-tab-overflow">
+            <button
+              className="bsds-tab-overflow-control"
+              disabled={scrollBtnStates[0]}
+              tabIndex={-1}
+              aria-hidden="true"
+              onClick={() => scrollTabs(-1)}>
+                <IconChevronLeft className="bsds-icon-2x" />
+            </button>
+            <button
+              className="bsds-tab-overflow-control"
+              disabled={scrollBtnStates[1]}
+              tabIndex={-1}
+              aria-hidden="true"
+              onClick={() => scrollTabs(1)}>
+                <IconChevronRight className="bsds-icon-2x" />
+            </button>
+          </div>
+        }
       </div>
       <div className="bsds-tab-panels">
         {tabPanels.map((tabPanel, index) => (
