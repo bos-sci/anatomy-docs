66d95d271a46d5a988d31dc727998560d044b063
APJohns <mail@ashpjohns.com>
2022-09-01 14:09:58 -0400

    Fix icon flashing

--- src/library/components/icon/Icon.tsx
+++ src/library/components/icon/Icon.tsx
@@ -1,6 +1,6 @@
 // TODO: figure out why bsds-icon-2x svgs also getting undefined
 
-import { lazy, Suspense } from 'react';
+import { lazy, ReactElement, Suspense, useEffect, useState } from 'react';
 
 import './Icon.scss';
 
@@ -21,63 +21,78 @@ const Fallback = () => {
 }
 
 const Icon = (props: Props): JSX.Element => {
+  const [sizeClass, setSizeClass] = useState('');
+  const [icon, setIcon] = useState<ReactElement>();
 
-  const sizeClass = `bsds-icon-${props.size ? props.size : '2x'}`;
+  useEffect(() => {
+    setSizeClass(`bsds-icon-${props.size ? props.size : '2x'}`);
+  }, [props.size]);
 
-  switch (props.name) {
-    case 'ellipsis':
-      const IconEllipsis = lazy(() => import('./icons/IconEllipsis'));
-      return (
-        <Suspense fallback={<Fallback />}>
-          <IconEllipsis className={sizeClass + ' ' + props.className} />
-        </Suspense>
-      );
-    case 'plus':
-      const IconPlus = lazy(() => import('./icons/IconPlus'));
-      return (
-        <Suspense fallback={<Fallback />}>
-          <IconPlus className={sizeClass + ' ' + props.className} />
-        </Suspense>
-      );
-    case 'menu':
-      const IconMenu = lazy(() => import('./icons/IconMenu'));
-      return (
-        <Suspense fallback={<Fallback />}>
-          <IconMenu className={sizeClass + ' ' + props.className} />
-        </Suspense>
-      );
-    case 'chevronRight':
-      const IconChevronRight = lazy(() => import('./icons/IconChevronRight'));
-      return (
-        <Suspense fallback={<Fallback />}>
-          <IconChevronRight className={sizeClass + ' ' + props.className} />
-        </Suspense>
-      );
-    case 'chevronLeft':
-      const IconChevronLeft = lazy(() => import('./icons/IconChevronLeft'));
-      return (
-        <Suspense fallback={<Fallback />}>
-          <IconChevronLeft className={sizeClass + ' ' + props.className} />
-        </Suspense>
-      );
-    case 'chevronDown':
-      const IconChevronDown = lazy(() => import('./icons/IconChevronDown'));
-      return (
-        <Suspense fallback={<Fallback />}>
-         <IconChevronDown className={sizeClass + ' ' + props.className} />
-        </Suspense>
-      );
-    case 'chevronUp':
-      const IconChevronUp = lazy(() => import('./icons/IconChevronUp'));
-      return (
-        <Suspense fallback={<Fallback />}>
-          <IconChevronUp className={sizeClass + ' ' + props.className} />
-        </Suspense>
-      );
+  useEffect(() => {
+    switch (props.name) {
+      case 'ellipsis':
+        const IconEllipsis = lazy(() => import('./icons/IconEllipsis'));
+        setIcon(
+          <Suspense fallback={<Fallback />}>
+            <IconEllipsis className={sizeClass + ' ' + props.className} />
+          </Suspense>
+        );
+        break;
+      case 'plus':
+        const IconPlus = lazy(() => import('./icons/IconPlus'));
+        setIcon(
+          <Suspense fallback={<Fallback />}>
+            <IconPlus className={sizeClass + ' ' + props.className} />
+          </Suspense>
+        );
+        break;
+      case 'menu':
+        const IconMenu = lazy(() => import('./icons/IconMenu'));
+        setIcon(
+          <Suspense fallback={<Fallback />}>
+            <IconMenu className={sizeClass + ' ' + props.className} />
+          </Suspense>
+        );
+        break;
+      case 'chevronRight':
+        const IconChevronRight = lazy(() => import('./icons/IconChevronRight'));
+        setIcon(
+          <Suspense fallback={<Fallback />}>
+            <IconChevronRight className={sizeClass + ' ' + props.className} />
+          </Suspense>
+        );
+        break;
+      case 'chevronLeft':
+        const IconChevronLeft = lazy(() => import('./icons/IconChevronLeft'));
+        setIcon(
+          <Suspense fallback={<Fallback />}>
+            <IconChevronLeft className={sizeClass + ' ' + props.className} />
+          </Suspense>
+        );
+        break;
+      case 'chevronDown':
+        const IconChevronDown = lazy(() => import('./icons/IconChevronDown'));
+        setIcon(
+          <Suspense fallback={<Fallback />}>
+           <IconChevronDown className={sizeClass + ' ' + props.className} />
+          </Suspense>
+        );
+        break;
+      case 'chevronUp':
+        const IconChevronUp = lazy(() => import('./icons/IconChevronUp'));
+        setIcon(
+          <Suspense fallback={<Fallback />}>
+            <IconChevronUp className={sizeClass + ' ' + props.className} />
+          </Suspense>
+        );
+        break;
 
-    default:
-      return <p>Failed to load icon</p>;
-  }
+      default:
+        setIcon(<p>Failed to load icon</p>);
+    }
+  }, [props.className, props.name, sizeClass]);
+
+  return <>{icon}</>;
 }
 
 export default Icon;
