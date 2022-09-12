b233d875bec6d1694555c7fcf0f31fa9c62221b8
cblva <chrisbuylova@gmail.com>
2022-09-12 14:43:14 -0400

    Default card example update

--- src/docs/components/variants/card/DefaultCard.tsx
+++ src/docs/components/variants/card/DefaultCard.tsx
@@ -5,7 +5,7 @@ const DefaultCard = (): JSX.Element => {
 
   return (
     <Example>
-      <Card hasImage={true} imageSrc="https://www.bostonscientific.com/en-US/Home/_jcr_content/root/container/container/container_1729237318/teaser_copy.coreimg.90.1600.jpeg/1660295012015/corp-featured-stories-mental-well-being.jpeg" imageAlt="This is card alt text" cardTitle='This is a card title'/>
+      <Card hasImage={true} imageSrc="https://www.bostonscientific.com/en-US/Home/_jcr_content/root/container/container/container_1729237318/teaser_copy.coreimg.90.1600.jpeg/1660295012015/corp-featured-stories-mental-well-being.jpeg" imageAlt="This is card alt text" cardTitle='This is a card title' />
     </Example>
   );
 }
