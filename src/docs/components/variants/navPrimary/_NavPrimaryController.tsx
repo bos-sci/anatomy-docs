5df377a5d35dca193f3c0eb2bc91b14548aab0cb
APJohns <mail@ashpjohns.com>
2022-09-15 15:31:26 -0400

    Add default and constrained nav examples

--- src/docs/components/variants/navPrimary/_NavPrimaryController.tsx
+++ src/docs/components/variants/navPrimary/_NavPrimaryController.tsx
@@ -1,17 +1,25 @@
 import { VariantProps } from '../Preview';
 import ComplexNavPrimary from './ComplexNavPrimary';
+import ConstrainedNavPrimary from './ConstrainedNavPrimary';
+import DefaultNavPrimary from './DefaultNavPrimary';
 import IntermediateNavPrimary from './IntermediateNavPrimary';
 import SimpleNavPrimary from './SimpleNavPrimary';
 
 const NavPrimaryController = ({ variantId }: VariantProps): JSX.Element => {
   switch (variantId) {
-    // Styles
+    // Modifiers
     case 'primaryNavigationSimple':
       return <SimpleNavPrimary />;
     case 'primaryNavigationIntermediate':
       return <IntermediateNavPrimary />;
     case 'primaryNavigationComplex':
-      return <ComplexNavPrimary />;
+        return <ComplexNavPrimary />;
+
+    // Styles
+    case 'primaryNavigationDefault':
+      return <DefaultNavPrimary />;
+    case 'primaryNavigationConstrained':
+      return <ConstrainedNavPrimary />;
 
     default:
       return <SimpleNavPrimary />;
