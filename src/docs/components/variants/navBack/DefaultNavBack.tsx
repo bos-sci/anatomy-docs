1ed62da7dc1375a7e9baa0382fbcc2900c8f19cb
APJohns <mail@ashpjohns.com>
2022-09-26 13:09:30 -0400

    add back button example and styles

---
+++ src/docs/components/variants/navBack/DefaultNavBack.tsx
@@ -0,0 +1,12 @@
+import Button from '../../../../library/components/Button';
+import Example from '../../../shared/components/Example';
+
+const DefaultNavBack = (): JSX.Element => {
+  return (
+    <Example>
+      <Button type="button" className="bsds-button-nav-back">Back</Button>
+    </Example>
+  );
+}
+
+export default DefaultNavBack;
