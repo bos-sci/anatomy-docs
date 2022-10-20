0f4db6c5c167151820733a38db15b29f4c9cd627
Eli Moreta-Feliz <eum113@gmail.com>
2022-10-20 16:08:46 -0400

    updated 'toSlug' to match 'slugify' purely

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,7 +2,19 @@ const environment = (envVar) => {
   return process.env[envVar] || "";
 };
 
-const toSlug = (string) => string.toLowerCase().replace(/\s+/g, "-");
+// toSlug is a copy of slugify in src/docs/shared/helpers.ts
+// Do not update without also updating slugify
+// @TODO: Make helpers.ts a CommonJS module or enable module loading for node.
+const toSlug = (string) =>
+  string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+
 module.exports = {
   environment,
   toSlug,
