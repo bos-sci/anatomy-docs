3802b250fe56a8fd823a8b970f905e3ba0b4a63d
APJohns <mail@ashpjohns.com>
2022-10-25 13:45:58 -0400

    add ids to md headings

--- src/docs/shared/components/Markdown.tsx
+++ src/docs/shared/components/Markdown.tsx
@@ -24,11 +24,20 @@ const Markdown = ({ markdown, headingOffset = 0, className }: Props): JSX.Elemen
       table.parentNode?.insertBefore(wrapperDiv, table);
       wrapperDiv.appendChild(table);
     });
-    mdDom.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(heading => {
-      if (!heading.id) {
-        heading.id = slugify(heading.textContent || '');
-      }
-    });
+
+    const headings = mdDom.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    if (headings.length > 0) {
+      const getLevel = (heading: Element): Number => Number(heading.tagName.charAt(1));
+      headings.forEach((h, index) => {
+        let steps = [h];
+        for (let i = index; i >= 0; i--) {
+          if (getLevel(headings[i]) < getLevel(steps[steps.length - 1])) {
+            steps.push(headings[i]);
+          }
+        }
+        h.id = slugify(Array.from(steps, s => s.textContent).join('_'));
+      });
+    }
 
     setCleanMarkdown(mdDom.body.innerHTML);
   }, [markdown, headingOffset]);
