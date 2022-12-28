83c57813d32198a7833819f581d9bd0d4c24cc7f
Eli Moreta-Feliz <eum113@gmail.com>
2022-12-28 15:53:13 -0500

    added support for prettier as well as typescript plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,19 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
     "plugin:react/recommended",
-    "standard-with-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
   ],
   overrides: [],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["react"],
   rules: {
-    semi: ["error", "always"],
-    quotes: ["error", "single"], // Do we want to enforce a given style here? For now, leaving as a warning.
     "react/default-props-match-prop-types": ["error"],
     "react/jsx-no-useless-fragment": ["warning"],
   },
