5b984ac2c47f061f93950bff6013175295c67d40
Eli Moreta-Feliz <eum113@gmail.com>
2023-01-05 14:43:52 -0500

    added some custom rules for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+import { deprecatedElements } from "utils/deprecatedElements";
+
 module.exports = {
   env: {
     browser: true,
@@ -18,7 +20,28 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
-    "react/default-props-match-prop-types": ["error"],
-    "react/jsx-no-useless-fragment": ["warning"],
+    // Legend: 0 = off, 1 = warning, 2 = error
+    "react/default-props-match-prop-types": 2,
+    "react/jsx-no-useless-fragment": 1,
+    "react/boolean-prop-naming": [
+      1,
+      {
+        rule: "^(is|has|should|can|did)[A-Z]([A-Za-z0-9]?)+",
+        message:
+          "Boolean props like ({{ propName }}) need to start with one of the following auxiliary verbs: is, has, should, can, did.",
+      },
+    ],
+    "react/forbid-elements": [1, { forbid: deprecatedElements() }],
+
+    "react/function-component-definition": [
+      1,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+
+    "react/hook-use-state": [1, { allowDestructuredState: false }],
+    "react/iframe-missing-sandbox": 1,
   },
 };
---
+++ src/utils/deprecatedElements.js
@@ -0,0 +1,35 @@
+export const deprecatedElements = () => {
+  const elements = [
+    "acronym",
+    "applet",
+    "bgsound",
+    "big",
+    "blink",
+    "center",
+    "content",
+    "dir",
+    "font",
+    "frame",
+    "frameset",
+    "image",
+    "keygen",
+    "marquee",
+    "menuitem",
+    "nobr",
+    "noembed",
+    "noframes",
+    "param",
+    "plaintext",
+    "rb",
+    "rtc",
+    "shadow",
+    "spacer",
+    "strike",
+    "tt",
+    "xmp",
+  ];
+  return elements.map((element) => ({
+    element: element,
+    message: `<${element}> is deprecated. Please use a different element. https://developer.mozilla.org/en-US/docs/Web/HTML/Element#obsolete_and_deprecated_elements`,
+  }));
+};
