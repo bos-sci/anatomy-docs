7d830bede2d71193b6a29b61c1eb869264b28852
cblva <chrisbuylova@gmail.com>
2023-02-06 17:44:54 -0500

    Add aria-labelledby to tablist.

--- src/docs/components/variants/tabs/OverflowTabs.tsx
+++ src/docs/components/variants/tabs/OverflowTabs.tsx
@@ -5,7 +5,7 @@ import Example from '../../../shared/components/Example';
 const OverflowTabs = (): JSX.Element => {
   return (
     <Example>
-      <Tabs ariaLabel="Demo overflow tabs">
+      <Tabs tablistLabel="Demo overflow tabs">
         <TabPanel tabName="Tab 1">Tab panel 1</TabPanel>
         <TabPanel tabName="Tab 2">Tab panel 2</TabPanel>
         <TabPanel tabName="Tab 3">Tab panel 3</TabPanel>
