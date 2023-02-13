13140f5891bda5db965de01159ecaa177d11df37
Eli Moreta-Feliz <eum113@gmail.com>
2023-02-13 10:47:46 -0500

    stylelint: Removed deprecated rules, and replaced rules with 'whitelist' and 'blacklist' with their inclusive counterparts

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,12 +29,12 @@ module.exports = {
   plugins: ["@typescript-eslint", "react"],
   rules: {
     // Legend: 0 = off, 1 = warn, 2 = error
-    "prettier/prettier": 0,
+    "prettier/prettier": 1,
     "curly": ["warn", "all"],
-    "react/default-props-match-prop-types": 0,
-    "react/jsx-no-useless-fragment": 0,
+    "react/default-props-match-prop-types": 2,
+    "react/jsx-no-useless-fragment": 1,
     "react/boolean-prop-naming": [
-      0,
+      1,
       {
         rule: "^(is|has|should|can|did)[A-Z]([A-Za-z0-9]?)+",
         message:
@@ -44,19 +44,19 @@ module.exports = {
     // "react/forbid-elements": [1, { forbid: deprecatedElements() }], -> Need to upgrade ESLint config to do this one this way.
 
     "react/function-component-definition": [
-      0,
+      1,
       {
         namedComponents: "arrow-function",
         unnamedComponents: "arrow-function",
       },
     ],
 
-    "react/hook-use-state": [0, { allowDestructuredState: false }],
-    "react/iframe-missing-sandbox": 0,
-    "react/jsx-fragments": [0, "syntax"],
-    "react/jsx-handler-names": 0,
+    "react/hook-use-state": [1, { allowDestructuredState: false }],
+    "react/iframe-missing-sandbox": 1,
+    "react/jsx-fragments": [1, "syntax"],
+    "react/jsx-handler-names": 1,
     "react/jsx-key": [
-      0,
+      1,
       {
         checkFragmentShorthand: true,
         checkKeyMustBeforeSpread: true,
@@ -64,7 +64,7 @@ module.exports = {
       },
     ],
     "react/jsx-no-bind": [
-      0,
+      1,
       {
         ignoreDOMComponents: false,
         ignoreRefs: false,
@@ -73,23 +73,23 @@ module.exports = {
         allowBind: false,
       },
     ],
-    "react/jsx-no-constructed-context-values": 0,
-    "react/jsx-no-leaked-render": [0, { validStrategies: ["coerce"] }],
+    "react/jsx-no-constructed-context-values": 1,
+    "react/jsx-no-leaked-render": [1, { validStrategies: ["coerce"] }],
     "react/jsx-no-target-blank": [
-      0,
+      1,
       {
         warnOnSpreadAttributes: true,
         forms: true,
       },
     ],
     "react/jsx-pascal-case": [
-      0,
+      1,
       {
         allowNamespace: true,
       },
     ],
     "react/jsx-sort-props": [
-      0,
+      1,
       {
         callbacksLast: true,
         shorthandLast: true,
@@ -98,45 +98,45 @@ module.exports = {
         reservedFirst: true,
       },
     ],
-    "react/no-array-index-key": 0,
-    "react/no-danger": 0,
-    "react/no-invalid-html-attribute": 0, // Figure out more options here
-    "react/no-this-in-sfc": 0,
-    "react/no-unstable-nested-components": 0,
+    "react/no-array-index-key": 1,
+    "react/no-danger": 1,
+    "react/no-invalid-html-attribute": 1, // Figure out more options here
+    "react/no-this-in-sfc": 1,
+    "react/no-unstable-nested-components": 1,
     // We should make this an error once the build runs.
     // Barring objections, I don't see a case where we'd want unused prop types.
-    "react/no-unused-prop-types": 0,
-    "react/prefer-es6-class": 0,
+    "react/no-unused-prop-types": 1,
+    "react/prefer-es6-class": 1,
     "react/self-closing-comp": [
-      0,
+      1,
       {
         component: true,
         html: false,
       },
     ],
-    "react/static-property-placement": 0,
-    "react/style-prop-object": 0,
-    "react/void-dom-elements-no-children": 0,
+    "react/static-property-placement": 1,
+    "react/style-prop-object": 1,
+    "react/void-dom-elements-no-children": 1,
     "jsx-a11y/anchor-ambiguous-text": [
-      0,
+      1,
       {
         words: ["read more"],
       },
     ],
-    "jsx-a11y/control-has-associated-label": 0,
-    "jsx-a11y/lang": 0,
-    "jsx-a11y/prefer-tag-over-role": 0,
+    "jsx-a11y/control-has-associated-label": 1,
+    "jsx-a11y/lang": 1,
+    "jsx-a11y/prefer-tag-over-role": 1,
     // The following rules from the "recommended" config have been lowered to "warning" for the sake of building the project.
     // Once we fix the issues, we'd want to turne the back to "error".
-    "react/no-unescaped-entities": 0,
-    "no-case-declarations": 0,
-    "@typescript-eslint/ban-types": 0,
-    "prefer-const": 0,
-    "react/display-name": 0,
-    "@typescript-eslint/no-empty-function": 0,
-    "@typescript-eslint/no-non-null-asserted-optional-chain": 0,
-    "jsx-a11y/interactive-supports-focus": 0,
-    "no-empty": 0,
-    "jsx-a11y/no-static-element-interactions": 0,
+    "react/no-unescaped-entities": 1,
+    "no-case-declarations": 1,
+    "@typescript-eslint/ban-types": 1,
+    "prefer-const": 1,
+    "react/display-name": 1,
+    "@typescript-eslint/no-empty-function": 1,
+    "@typescript-eslint/no-non-null-asserted-optional-chain": 1,
+    "jsx-a11y/interactive-supports-focus": 1,
+    "no-empty": 1,
+    "jsx-a11y/no-static-element-interactions": 1,
   },
 };
