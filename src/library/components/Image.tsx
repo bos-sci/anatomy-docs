6ee87726d86f45feba965575bf9eefc0daa8e828
Eli Moreta-Feliz <eum113@gmail.com>
2023-02-20 14:10:02 -0500

    re-added comments on Image.tsx

--- src/library/components/Image.tsx
+++ src/library/components/Image.tsx
@@ -1,37 +1,44 @@
 import { ImgHTMLAttributes, useId } from "react";
 
 interface BaseProps extends ImgHTMLAttributes<HTMLImageElement> {
-  ratio?: Ratio,
-  hasCaption?: boolean,
-  isCaptionCentered?: boolean,
-  isGhost?: boolean,
+  ratio?: Ratio;
+  hasCaption?: boolean;
+  isCaptionCentered?: boolean;
+  isGhost?: boolean;
   texts?: {
-    caption?: string
-  }
+    caption?: string;
+  };
 }
 
 type AltTextProps =
-| {
-  isDecorative: boolean,
-  alt?: never
-}
-| {
-  isDecorative?: never,
-  alt: string
-}
+  | {
+      isDecorative: boolean;
+      alt?: never;
+    }
+  | {
+      isDecorative?: never;
+      alt: string;
+    };
 
 export type Ratio = "1:1" | "4:3" | "16:9" | "21:9";
 export type Props = BaseProps & AltTextProps;
 
-const Image = (props : Props): JSX.Element => {
-  const {alt, ratio = "16:9", isDecorative, hasCaption, texts, className, ...imgAttrs} = props;
-
+const Image = (props: Props): JSX.Element => {
+  const {
+    alt,
+    ratio = "16:9",
+    isDecorative,
+    hasCaption,
+    texts,
+    className,
+    ...imgAttrs // Warning on the test file stems from spreading props here.
+  } = props;
   const captionId = useId();
   const imageId = useId();
 
   let ratioClasses = "";
 
-  switch(ratio) {
+  switch (ratio) {
     case "1:1":
       ratioClasses = "-1to1";
       break;
@@ -44,7 +51,7 @@ const Image = (props : Props): JSX.Element => {
     case "21:9":
       ratioClasses = "-21to9";
       break;
-  };
+  }
 
   let captionAlignment = "";
 
@@ -59,20 +66,27 @@ const Image = (props : Props): JSX.Element => {
   return (
     <>
       <img
-        className={`bsds-image${ratioClasses}${className ? " " + className : ""}`}
+        className={`bsds-image${ratioClasses}${
+          className ? " " + className : ""
+        }`}
         alt={isDecorative ? "" : alt}
         id={"image" + imageId}
-        aria-describedby={hasCaption && texts?.caption ? `imageCaption${captionId}` : undefined}
+        aria-describedby={
+          hasCaption && texts?.caption ? `imageCaption${captionId}` : undefined
+        }
+        // As a result, isGhost and isCaptionCentered are making it here.
         {...imgAttrs}
       />
-      { hasCaption && texts?.caption &&
-        <p className={`${"bsds-image-caption"}${captionAlignment}`} id={"imageCaption" + captionId}>
+      {hasCaption && texts?.caption && (
+        <p
+          className={`${"bsds-image-caption"}${captionAlignment}`}
+          id={"imageCaption" + captionId}
+        >
           {texts?.caption}
         </p>
-      }
+      )}
     </>
-  )
-
-}
+  );
+};
 
 export default Image;
