1cd375b2977935c5ad25007c0e58de726b0ec5fa
cblva <chrisbuylova@gmail.com>
2023-03-28 14:58:37 -0400

    Add useId to svg paths

--- src/library/components/icon/icons/IconCardDemo.tsx
+++ src/library/components/icon/icons/IconCardDemo.tsx
@@ -1,12 +1,25 @@
 import { IconProps } from './iconTypes';
+import { useId } from 'react';
 
 const IconCardDemo = (props: IconProps): JSX.Element => {
-  return <svg aria-hidden="true" focusable="false" className={`${props.className}`} data-testid="cardDemoIcon" xmlns="http://www.w3.org/2000/svg"
-  x="0px" y="0px" viewBox="0 0 512 384">
-<path d="M464,0H48C21.5,0,0,21.5,0,48v288c0,26.5,21.5,48,48,48h416c26.5,0,48-21.5,48-48V48C512,21.5,490.5,0,464,0z M458,336H54
- c-3.3,0-6-2.7-6-6V54c0-3.3,2.7-6,6-6h404c3.3,0,6,2.7,6,6v276C464,333.3,461.3,336,458,336z M128,88c-22.1,0-40,17.9-40,40
- s17.9,40,40,40s40-17.9,40-40S150.1,88,128,88z M96,288h320v-80l-87.5-87.5c-4.7-4.7-12.3-4.7-17,0L192,240l-39.5-39.5
- c-4.7-4.7-12.3-4.7-17,0L96,240V288z"/></svg>;
-}
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      className={`${props.className}`}
+      data-testid="cardDemoIcon"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      viewBox="0 0 512 384"
+    >
+      <path
+        id={`icon${useId()}`}
+        d="M464,0H48C21.5,0,0,21.5,0,48v288c0,26.5,21.5,48,48,48h416c26.5,0,48-21.5,48-48V48C512,21.5,490.5,0,464,0z M458,336H54c-3.3,0-6-2.7-6-6V54c0-3.3,2.7-6,6-6h404c3.3,0,6,2.7,6,6v276C464,333.3,461.3,336,458,336z M128,88c-22.1,0-40,17.9-40,40s17.9,40,40,40s40-17.9,40-40S150.1,88,128,88z M96,288h320v-80l-87.5-87.5c-4.7-4.7-12.3-4.7-17,0L192,240l-39.5-39.5c-4.7-4.7-12.3-4.7-17,0L96,240V288z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
 
 export default IconCardDemo;
--- src/library/components/icon/icons/IconChevronLeft.tsx
+++ src/library/components/icon/icons/IconChevronLeft.tsx
@@ -1,7 +1,23 @@
 import { IconProps } from './iconTypes';
+import { useId } from 'react';
 
 const IconChevronLeft = (props: IconProps): JSX.Element => {
-  return <svg aria-hidden="true" focusable="false" className={`${props.className}`} role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path fill="currentColor" d="M3.83,8.3l8-8a1,1,0,0,1,1.4,0l.93.93a1,1,0,0,1,0,1.4L7.82,9l6.35,6.38a1,1,0,0,1,0,1.4l-.93.93a1,1,0,0,1-1.4,0l-8-8A1,1,0,0,1,3.83,8.3Z"/></svg>;
-}
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      className={`${props.className}`}
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 18 18"
+    >
+      <path
+        id={`icon${useId()}`}
+        d="M3.83,8.3l8-8a1,1,0,0,1,1.4,0l.93.93a1,1,0,0,1,0,1.4L7.82,9l6.35,6.38a1,1,0,0,1,0,1.4l-.93.93a1,1,0,0,1-1.4,0l-8-8A1,1,0,0,1,3.83,8.3Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
 
 export default IconChevronLeft;
