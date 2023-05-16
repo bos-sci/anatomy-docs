1c894df3a70cbd71c2d8f994a8c3e046deb97012
cblva <chrisbuylova@gmail.com>
2023-05-16 15:20:09 -0400

    Add subtle Tag style

---
+++ src/docs/components/variants/tag/SubtleStyle.tsx
@@ -0,0 +1,19 @@
+import Tag from '../../../../library/components/Tag';
+import Example from '../../../shared/components/Example';
+
+const SubtleStyle = (): JSX.Element => {
+  return (
+    <>
+      <Example>
+        <Tag variant="subtle">Subtle tag</Tag>
+      </Example>
+      <Example isDarkTheme>
+        <Tag variant="subtle" isGhost>
+          Subtle tag
+        </Tag>
+      </Example>
+    </>
+  );
+};
+
+export default SubtleStyle;
