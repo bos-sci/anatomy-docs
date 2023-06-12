ac22852731b5a6cdae89f21e3bcf13bd74374338
APJohns <mail@ashpjohns.com>
2023-06-12 14:48:54 -0400

    fix version update link to use latest version

--- src/utils/updateTokensVersion.js
+++ src/utils/updateTokensVersion.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const axios = require('axios');
 
 const updateTokens = async () => {
-  const res = await axios.get('https://cdn.jsdelivr.net/npm/@boston-scientific/anatomy-tokens@1.0.0/package.json');
+  const res = await axios.get('https://cdn.jsdelivr.net/npm/@boston-scientific/anatomy-tokens/package.json');
   const version = res.data.version;
   fs.writeFileSync('src/docs/shared/versions.ts', `export const tokens = '${version}';\n`);
 };
