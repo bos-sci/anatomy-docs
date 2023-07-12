c693b412cb10796a6ae4fa1eaffa8c709e81010c
Kasey Labelle <kasey.l.labelle@gmail.com>
2023-07-12 11:57:00 -0400

    fix accordion headings

--- src/docs/pages/components/variants/accordion/ContainedAccordion.tsx
+++ src/docs/pages/components/variants/accordion/ContainedAccordion.tsx
@@ -5,10 +5,10 @@ import Example from 'docs/shared/components/Example';
 const ContainedAccordion = (): JSX.Element => {
   return (
     <Example>
-      <Accordion headingLevel="h2" isContained>
-        <AccordionPanel heading="Accordion 1">Accordion panel 1</AccordionPanel>
-        <AccordionPanel heading="Accordion 2">Accordion panel 2</AccordionPanel>
-        <AccordionPanel heading="Accordion 3">Accordion panel 3</AccordionPanel>
+      <Accordion headingLevel="h4" isContained>
+        <AccordionPanel heading="Contained accordion 1">Accordion panel 1</AccordionPanel>
+        <AccordionPanel heading="Contained accordion 2">Accordion panel 2</AccordionPanel>
+        <AccordionPanel heading="Contained accordion 3">Accordion panel 3</AccordionPanel>
       </Accordion>
     </Example>
   );
