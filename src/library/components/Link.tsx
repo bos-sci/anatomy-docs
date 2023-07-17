7e4a84777da48906097fe320f806d8626f9356f0
cblva <chrisbuylova@gmail.com>
2023-07-17 17:45:31 -0400

    implement feedback, fix undefined

--- src/library/components/Link.tsx
+++ src/library/components/Link.tsx
@@ -79,7 +79,14 @@ const Link = forwardRef(
     } else {
       return (
         // eslint-disable-next-line react/jsx-no-target-blank
-        <a ref={ref} href={href} className={`${classes} ${className}`} target={target} rel={relAttr} {...linkAttrs}>
+        <a
+          ref={ref}
+          href={href}
+          className={`${classes} ${className ? className : ''}`}
+          target={target}
+          rel={relAttr}
+          {...linkAttrs}
+        >
           {children}
         </a>
       );
