d02bce8c48a71edde2dc6fffd6cb572f936fc2b2
cblva <chrisbuylova@gmail.com>
2023-08-18 14:03:11 -0400

    Fix test

--- src/library/components/__tests__/TextArea.test.tsx
+++ src/library/components/__tests__/TextArea.test.tsx
@@ -1,19 +1,19 @@
 import { render, screen } from '@testing-library/react';
-import TextArea from 'library/components/Textarea';
+import Textarea from 'library/components/Textarea';
 
-describe('TextArea', () => {
+describe('Textarea', () => {
   it('Renders a blank input field by default', () => {
-    render(<TextArea label="Default Input" />);
+    render(<Textarea label="Default Input" />);
     expect(screen.getByLabelText('Default Input')).toBeInTheDocument();
   });
   it("renders an error node when 'forceValidation' is true and 'errorText' is present", () => {
-    render(<TextArea label="Error Sample" errorText="Error Text" forceValidation />);
+    render(<Textarea label="Error Sample" errorText="Error Text" forceValidation />);
     expect(screen.getByText('Error Text')).toBeInTheDocument();
   });
   it("does not render an error node if 'forceValidation' is false", () => {
     // use queryBy over getBy when testing for the absence of an element
     // https://testing-library.com/docs/guide-disappearance/#asserting-elements-are-not-present
-    render(<TextArea label="Error Sample" errorText="Error Text" />);
+    render(<Textarea label="Error Sample" errorText="Error Text" />);
     expect(screen.queryByText('Error Text')).not.toBeInTheDocument();
   });
 });
