98a1b4880536848999ae1d8466a40ecceb9fc588
Kasey Labelle <kasey.l.labelle@gmail.com>
2023-10-09 16:31:24 -0400

    remove inline icon from cta style link

--- src/pages/components/variants/link/CtaStyle.tsx
+++ src/pages/components/variants/link/CtaStyle.tsx
@@ -1,13 +1,11 @@
 import Example from 'shared/components/Example';
 import { Link } from '@boston-scientific/anatomy-react';
-import { IconArrowRight } from '@boston-scientific/anatomy-react';
 
 const CtaStyle = (): JSX.Element => {
   return (
     <Example>
       <Link href="docs-demo-link" variant="cta">
         CTA link
-        <IconArrowRight className="bsds-link-cta-icon" />
       </Link>
     </Example>
   );
