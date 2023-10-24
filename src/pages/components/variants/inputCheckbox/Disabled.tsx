f0904100b49d80b86e05b0b87a8e7d2120310f9e
cblva <chrisbuylova@gmail.com>
2023-10-24 15:04:03 -0400

    Add Checkbox group previews to Checkbox page

--- src/pages/components/variants/inputCheckbox/Disabled.tsx
+++ src/pages/components/variants/inputCheckbox/Disabled.tsx
@@ -1,16 +1,26 @@
 import { InputCheckbox } from '@boston-scientific/anatomy-react';
+import { Fieldset } from '@boston-scientific/anatomy-react';
 import Example from 'shared/components/Example';
 
 const Disabled = (): JSX.Element => {
   return (
-    <Example>
-      <div className="bsds-form-control">
-        <InputCheckbox label="Disabled checkbox" disabled />
-      </div>
-      <div className="bsds-form-control">
-        <InputCheckbox label="Disabled checked checkbox" disabled defaultChecked />
-      </div>
-    </Example>
+    <>
+      <Example>
+        <div className="bsds-form-control">
+          <InputCheckbox label="Disabled checkbox" disabled />
+        </div>
+        <div className="bsds-form-control">
+          <InputCheckbox label="Disabled checked checkbox" disabled defaultChecked />
+        </div>
+      </Example>
+      <Example>
+        <Fieldset legend="Disabled checkbox group" disabled>
+          <InputCheckbox label="Checkbox 1" defaultChecked />
+          <InputCheckbox label="Checkbox 2" />
+          <InputCheckbox label="Checkbox 3" />
+        </Fieldset>
+      </Example>
+    </>
   );
 };
 
--- src/pages/components/variants/inputCheckbox/WithHelp.tsx
+++ src/pages/components/variants/inputCheckbox/WithHelp.tsx
@@ -1,21 +1,34 @@
 import { useState } from 'react';
 import { InputCheckbox } from '@boston-scientific/anatomy-react';
 import Example from 'shared/components/Example';
+import { Fieldset } from '@boston-scientific/anatomy-react';
 
 const WithHelp = (): JSX.Element => {
   const [isChecked, setIsChecked] = useState(true);
 
   return (
-    <Example>
-      <div className="bsds-form-control">
-        <InputCheckbox
-          label="Checkbox"
+    <>
+      <Example>
+        <div className="bsds-form-control">
+          <InputCheckbox
+            label="Checkbox"
+            helpText="This is an example of help text. It can wrap to two lines, but try not to go longer than three."
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+          />
+        </div>
+      </Example>
+      <Example>
+        <Fieldset
+          legend="Checkbox group"
           helpText="This is an example of help text. It can wrap to two lines, but try not to go longer than three."
-          checked={isChecked}
-          onChange={() => setIsChecked(!isChecked)}
-        />
-      </div>
-    </Example>
+        >
+          <InputCheckbox label="Checkbox 1" />
+          <InputCheckbox label="Checkbox 2" />
+          <InputCheckbox label="Checkbox 3" />
+        </Fieldset>
+      </Example>
+    </>
   );
 };
 
