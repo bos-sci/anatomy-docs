de12af48abac5b82c4565bbc49d611639544f163
Ashley Johns <mail@ashpjohns.com>
2024-01-24 13:50:18 -0500

    add pagination component

---
+++ src/pages/components/variants/pagination/DefaultPagination.tsx
@@ -0,0 +1,12 @@
+import { Pagination } from '@boston-scientific/anatomy-react';
+import Example from 'shared/components/Example';
+
+const DefaultPagination = () => {
+  return (
+    <Example>
+      <Pagination numberOfPages={10} />
+    </Example>
+  );
+};
+
+export default DefaultPagination;
